0e602d1f149e0fa3144e697777bad913234c17b9
Akshat Sharma <akshatksharma@wustl.edu>
2021-09-27 12:51:14 -0500

    Removed theming from head for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,7 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        theme_color_in_head: false,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
